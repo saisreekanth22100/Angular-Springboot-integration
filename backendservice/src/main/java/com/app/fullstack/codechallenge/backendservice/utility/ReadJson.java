997d03e10b7f5a3f92e19ef907f8d3a91a9d4430
saisreekanth yellisetty <saisreekanth22100@gmail.com>
2021-02-13 23:44:00 +0100

    main : Json data inserted need to trigger manually. No resource exposed to trigger

--- backendservice/src/main/java/com/app/fullstack/codechallenge/backendservice/utility/ReadJson.java
+++ backendservice/src/main/java/com/app/fullstack/codechallenge/backendservice/utility/ReadJson.java
@@ -1,21 +1,22 @@
 package com.app.fullstack.codechallenge.backendservice.utility;
 
+import com.app.fullstack.codechallenge.backendservice.model.Car;
+import com.app.fullstack.codechallenge.backendservice.repository.CarRepositoryImpl;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
 public class ReadJson {
 
-    public static void main(String[] args) {
-        iterateJson();
-    }
-
-    public static void iterateJson(){
+    @Autowired
+    private CarRepositoryImpl carRepositoryImpl;
+    public void iterateJson(){
 
         try (JsonParser jParser = new JsonFactory()
                 .createParser(new ClassPathResource("/Cars.json").getFile());) {
@@ -24,36 +25,7 @@ public class ReadJson {
 
                 while (jParser.nextToken() != JsonToken.END_ARRAY) {
 
-                    // loop until token equal to "}"
-                    while (jParser.nextToken() != JsonToken.END_OBJECT) {
-
-                        String fieldname = jParser.getCurrentName();
-                        if ("name".equals(fieldname)) {
-                            // current token is "name",
-                            // move to next, which is "name"'s value
-                            jParser.nextToken();
-                            System.out.println(jParser.getText());
-                        }
-
-                        if ("age".equals(fieldname)) {
-                            jParser.nextToken();
-                            System.out.println(jParser.getIntValue());
-                        }
-
-                        if ("messages".equals(fieldname)) {
-
-                            //jParser.nextToken(); // current token is "[", move next
-                            if (jParser.nextToken() == JsonToken.START_ARRAY) {
-                                // messages is array, loop until token equal to "]"
-                                while (jParser.nextToken() != JsonToken.END_ARRAY) {
-                                    System.out.println(jParser.getText());
-                                }
-                            }
-
-                        }
-
-                    }
-
+                    carRepositoryImpl.insertWithQuery(jParser.readValueAs(Car.class));
                 }
             }
 
