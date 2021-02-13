bad09e0b5051fb11c9b504c514b8947265628a88
saisreekanth yellisetty <saisreekanth22100@gmail.com>
2021-02-13 19:33:06 +0100

    springboot initializer commit

---
+++ backendservice/src/main/java/com/app/fullstack/codechallenge/backendservice/BackendserviceApplication.java
@@ -0,0 +1,13 @@
+package com.app.fullstack.codechallenge.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendserviceApplication.class, args);
+	}
+
+}
